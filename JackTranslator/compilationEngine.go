fbe1a9faa222e612c20ae858159df034c5f191fd
zabrown2000 <zabrown2000@gmail.com>
2024-06-11 14:35:04 +0300

    Added skeleton for new functions

--- JackTranslator/compilationEngine.go
+++ JackTranslator/compilationEngine.go
@@ -1,1 +1,70 @@
 package JackTranslator
+
+import (
+	"bufio"
+)
+
+type CompilationEngine struct {
+	tokenizer *JackTokenizer
+	writer    *bufio.Writer
+}
+
+func NewCompilationEngine(inputFile string, outputFile string) *CompilationEngine {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileClass() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileClassVarDec() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileSubroutine() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileParameterList() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileVarDec() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileStatements() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileDo() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileLet() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileWhile() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileReturn() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileIf() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileExpression() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileTerm() {
+	// fill in here
+}
+
+func (ce *CompilationEngine) compileExpressionList() {
+	// fill in here
+}
