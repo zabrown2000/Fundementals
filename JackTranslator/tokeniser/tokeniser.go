66b1315b3e8727f7cad69f1839b1d3cdf0db80d1
talcohen2021 <talcohen@g.jct.ac.il>
2024-06-23 21:17:16 +0300

    implemented tokeniseline and helper functions

--- JackTranslator/tokeniser/tokeniser.go
+++ JackTranslator/tokeniser/tokeniser.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
-
-var keywords []string
-var symbols []string
 
 // throws if illegal token
 // use lexical elements section from slides
@@ -29,10 +28,13 @@ type Token struct {
 }
 
 type Tokeniser struct {
-	file        *os.File
-	currentLine string
-	reader      *bufio.Reader
-	tokens      *[]Token
+	file         *os.File
+	currentLine  string
+	reader       *bufio.Reader
+	tokens       []Token
+	lengthTokens int
+	keywordsMap  map[string]bool
+	symbolsMap   map[string]bool
 }
 
 // New creates a new tokeniser instance for a given file path
@@ -79,9 +81,9 @@ func (t *Tokeniser) parseNextLine() {
 	return // Exit the function after updating currentLine
 }
 func (t *Tokeniser) init() {
-	keywords = []string{"class", "constructor", "method", "function", "field", "static", "var", "int", "char", "boolean", "void", "true", "false", "null",
+	keywords := []string{"class", "constructor", "method", "function", "field", "static", "var", "int", "char", "boolean", "void", "true", "false", "null",
 		"this", "let", "do", "if", "else", "while", "return"}
-	symbols = []string{"{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~"}
+	symbols := []string{"{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~"}
 	//Map for keywords
 	keywordsMap := make(map[string]bool)
 	for _, v := range keywords {
@@ -105,26 +107,81 @@ func (t *Tokeniser) TokenType() int {
 	return 0 // fill in here
 }
 
-func (t *Tokeniser) KeyWord() int {
-	return 0 // fill in here
+/*
+	func (t *Tokeniser) KeyWord() int {
+		return 0 // fill in here
+	}
+
+	func (t *Tokeniser) Symbol() byte {
+		return 0 // fill in here
+	}
+*/
+func (t *Tokeniser) Identifier(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	re := regexp.MustCompile(`^[a-zA-Z_]`)
+	if !re.MatchString(s) {
+		return false
+	}
+	for i, r := range s {
+		if i == 0 {
+			continue
+		}
+		if !((r > 'a' && r < 'z') || (r > 'A' && r < 'Z') || (r > '0' && r < '9') || r == '_') {
+			return false
+		}
+
+	}
+	return true
 }
 
-func (t *Tokeniser) Symbol() byte {
-	return 0 // fill in here
+func (t *Tokeniser) IntVal(s string) bool {
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		return false // Conversion failed, not a valid integer
+	}
+
+	// Check if the integer is within the specified range [0, 32767]
+	return num >= 0 && num <= 32767
 }
 
-func (t *Tokeniser) Identifier() string {
-	return "" // fill in here
+func (t *Tokeniser) StringVal(s string) bool {
+	return len(s) > 1 && strings.HasPrefix(s, "\"") && strings.HasSuffix(s, "\"")
 }
 
-func (t *Tokeniser) IntVal() int {
-	return 0 // fill in here
-}
+func (t *Tokeniser) TokeniseLine() {
+	chars := []rune(t.currentLine)
+	cur_char := ""
+	token_candidate := ""
 
-func (t *Tokeniser) StringVal() string {
-	return "" // fill in here
-}
+	for i := 0; i <= len(chars); i++ {
+		if i < len(chars) {
+			cur_char += string(chars[i])
+		}
+		if t.keywordsMap[token_candidate] && !t.Identifier(cur_char) {
+			t.tokens = append(t.tokens, Token{KEYWORD, token_candidate})
+			token_candidate = ""
+		} else if t.symbolsMap[token_candidate] && (token_candidate != "/" || cur_char != "*") {
+			t.tokens = append(t.tokens, Token{SYMBOL, token_candidate})
+			token_candidate = ""
+		} else if t.StringVal(token_candidate) {
+			t.tokens = append(t.tokens, Token{STRING_CONST, token_candidate[1 : len(token_candidate)-1]})
+		} else if t.Identifier(token_candidate) && !((cur_char > "a" && cur_char < "z") || (cur_char > "A" && cur_char < "Z") || (cur_char > "0" && cur_char < "9") || cur_char == "_") {
+			t.tokens = append(t.tokens, Token{IDENTIFIER, token_candidate})
+			token_candidate = ""
+		} else if t.IntVal(token_candidate) && !(cur_char > "0" && cur_char < "9") {
+			t.tokens = append(t.tokens, Token{INT_CONST, token_candidate})
+			token_candidate = ""
+		}
 
-func (t *Tokeniser) TokeniseLine() int {
-	return 0
+		if cur_char == "0" {
+			// new line chars always skip
+		} else if cur_char == " " && !strings.HasPrefix(token_candidate, "\"") {
+			// is regular space, skip
+		} else {
+			// append new char
+			token_candidate += cur_char
+		}
+	}
 }
