b51bb4834e3f54e68fbc97e652d47c0fad34ea1d
talcohen2021 <talcohen@g.jct.ac.il>
2024-07-11 21:01:21 +0300

    Add Symbol, SymbolTable and Constructor

--- JackTranslator/symbolTable/symbolTable.go
+++ JackTranslator/symbolTable/symbolTable.go
@@ -1,6 +1,7 @@
 package symbolTable
 
 /*
+//do we need an interface?
 constructor
 startSubroutine
 define(name, type, kind)
@@ -9,3 +10,27 @@ kindOf(name) kind
 typeOf(name) String
 indexOf(name) int
 */
+
+// Symbol represents a single symbol in the symbol table
+type Symbol struct {
+	Name  string
+	Type  string
+	Kind  string
+	Index int
+}
+
+// SymbolTable manages the symbols for a class and subroutine scope
+type SymbolTable struct {
+	classScope      map[string]Symbol
+	subroutineScope map[string]Symbol
+	indexCounters   map[string]int
+}
+
+// NewSymbolTable creates a new symbol table - sets all indices to 0
+func NewSymbolTable() *SymbolTable {
+	return &SymbolTable{
+		classScope:      make(map[string]Symbol),
+		subroutineScope: make(map[string]Symbol),
+		indexCounters:   map[string]int{"static": 0, "field": 0, "argument": 0, "local": 0},
+	}
+}
