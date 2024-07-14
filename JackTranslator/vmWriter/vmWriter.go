68f74bf72dcd8ea9af8a796b401d09a168f5fa48
talcohen2021 <talcohen@g.jct.ac.il>
2024-07-14 18:45:42 +0300

    Program flow commands

--- JackTranslator/vmWriter/vmWriter.go
+++ JackTranslator/vmWriter/vmWriter.go
@@ -18,7 +18,6 @@ writeFunction(name, nlocals)
 writeReturn
 */
 
-// Stack manipulation commands
 func push(file *os.File, segment string, index int) {
 	_, err := fmt.Fprintf(file, "push %s %d\n", segment, index)
 	if err != nil {
@@ -28,6 +27,27 @@ func push(file *os.File, segment string, index int) {
 
 func pop(file *os.File, segment string, index int) {
 	_, err := fmt.Fprintf(file, "pop %s %d\n", segment, index)
+	if err != nil {
+		return
+	}
+}
+
+func label(file *os.File, labelName string) {
+	_, err := fmt.Fprintf(file, "label %s\n", labelName)
+	if err != nil {
+		return
+	}
+}
+
+func goTo(file *os.File, labelName string) {
+	_, err := fmt.Fprintf(file, "goto %s\n", labelName)
+	if err != nil {
+		return
+	}
+}
+
+func ifGoto(file *os.File, labelName string) {
+	_, err := fmt.Fprintf(file, "if-goto %s\n", labelName)
 	if err != nil {
 		return
 	}
