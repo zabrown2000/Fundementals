cc67b56d9ac98ef37e1abedd27db2c747faa8008
talcohen2021 <talcohen@g.jct.ac.il>
2024-07-14 18:44:44 +0300

    Stack manipulation commands

--- JackTranslator/vmWriter/vmWriter.go
+++ JackTranslator/vmWriter/vmWriter.go
@@ -1,5 +1,10 @@
 package vmWriter
 
+import (
+	"fmt"
+	"os"
+)
+
 /*
 constructor(file stream)
 writePush(segment)
@@ -12,3 +17,18 @@ writeCall(name, nargs)
 writeFunction(name, nlocals)
 writeReturn
 */
+
+// Stack manipulation commands
+func push(file *os.File, segment string, index int) {
+	_, err := fmt.Fprintf(file, "push %s %d\n", segment, index)
+	if err != nil {
+		return
+	}
+}
+
+func pop(file *os.File, segment string, index int) {
+	_, err := fmt.Fprintf(file, "pop %s %d\n", segment, index)
+	if err != nil {
+		return
+	}
+}
